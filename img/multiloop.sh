ffda2da951fad52911fe95e692f7b7be6d479cf5
pollenjp <polleninjp@gmail.com>
2018-05-13 07:17:19 +0000

    [Add] loop shell script

---
+++ img/multiloop.sh
@@ -0,0 +1,8 @@
+
+
+while :
+do
+    #raspistill -o sample.jpg
+    #python3 ${HOME}/dl_ans/tools/yolo_conv.py sample.jpg
+    ${HOME}/dl_ans/c_neon/c_neon
+done
---
+++ img/oneloop.sh
@@ -0,0 +1,7 @@
+
+
+raspistill -o sample.jpg
+
+python3 ${HOME}/dl_ans/tools/yolo_conv.py sample.jpg
+
+${HOME}/dl_ans/c_neon/c_neon
